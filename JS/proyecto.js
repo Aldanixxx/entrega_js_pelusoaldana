bfe31d4113e863f9520dba895c4aac5ed69ede77
Aldana Peluso <a.peluso1990@gmail.com>
2023-02-13 01:14:30 +0100

    first commit

---
+++ JS/proyecto.js
@@ -0,0 +1,111 @@
+
+// PRODUCTOS
+const productos = [
+    // Geles
+    {
+        id: "gel_1",
+        titulo: "Gel polish 1",
+        imagen: "./img/img7.jpg",
+        categoria: {
+            nombre: "Geles",
+            id: "geles"
+        },
+        precio: 1000
+    },
+    {
+        id: "gel_1",
+        titulo: "Gel polish 2",
+        imagen: "./img/img8.jpg",
+        categoria: {
+            nombre: "Geles",
+            id: "geles"
+        },
+        precio: 1000
+    },
+    {
+        id: "gel_1",
+        titulo: "Gel polish 3",
+        imagen: "./img/img9.jpg",
+        categoria: {
+            nombre: "Geles",
+            id: "geles"
+        },
+        precio: 1000
+    },
+    {
+        id: "gel_1",
+        titulo: "Gel polish 4",
+        imagen: "./img/img10.jpg",
+        categoria: {
+            nombre: "Geles",
+            id: "geles"
+        },
+        precio: 1000
+    },
+
+    // Esmaltes
+    {
+        id: "esmalte1",
+        titulo: "Esmalte 1",
+        imagen: "./img/img1.jpg",
+        categoria: {
+            nombre: "Esmaltes",
+            id: "esmaltes"
+        },
+        precio: 1000
+    },
+    {
+        id: "esmalte2",
+        titulo: "Esmalte 2",
+        imagen: "./img/img2.jpg",
+        categoria: {
+            nombre: "Esmaltes",
+            id: "esmaltes"
+        },
+        precio: 1000
+    },
+    {
+        id: "esmalte3",
+        titulo: "Esmalte 3",
+        imagen: "./img/img3.jpg",
+        categoria: {
+            nombre: "Esmaltes",
+            id: "esmaltes"
+        },
+        precio: 1000
+    },
+    {
+        id: "esmalte4",
+        titulo: "Esmalte 4",
+        imagen: "./img/img4.jpg",
+        categoria: {
+            nombre: "Esmaltes",
+            id: "esmaltes"
+        },
+        precio: 1000
+    },
+    
+    // Acrilicos y poligel
+    {
+        id: "acripoly1",
+        titulo: "Kit dip",
+        imagen: "./img/img5.jpg",
+        categoria: {
+            nombre: "Acrilicos y polygel",
+            id: "acripoly"
+        },
+        precio: 1000
+    },
+    
+    // Herramientas
+    {
+        id: "minitorno",
+        titulo: "Mini torno",
+        imagen: "./img/img6.jpg",
+        categoria: {
+            nombre: "Herramientas",
+            id: "herramientas"
+        },
+        precio: 1000
+    },
+]
